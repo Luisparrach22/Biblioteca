1704a03cdba06919180eca7ebb3322537d1340c4
Fabricio Caceres <fabriciocaceres422@gmail.com>
2026-09-22 16:46:09 +0200

    llamado de metodos

--- Trabajo1/src/ejercicio/repository/LibroRepository.java
+++ Trabajo1/src/ejercicio/repository/LibroRepository.java
@@ -1,5 +1,25 @@
 package ejercicio.repository;
 
-public class LibroRepository {
 
+import java.util.List;
+
+import ejercicio.model.Libro;
+
+public interface LibroRepository {
+
+    List<Libro> obtenerTodos();
+
+    List<Libro> buscarPorTitulo(String titulo);
+
+    List<Libro> buscarPorAutor(String autor);
+
+    List<Libro> buscarPorRangoPrecio(double precioMin, double precioMax);
+
+    List<Libro> buscarPorStockMinimo(int stockMinimo);
+
+    boolean insertar(Libro libro);
+
+    boolean eliminarPorTitulo(String titulo);
+
+    boolean copiar(LibroRepository destino);
 }

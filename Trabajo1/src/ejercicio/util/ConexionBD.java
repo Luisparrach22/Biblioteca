46941063781c59f6e1d9bf800932080f5f9e32c7
Luis Parra <parra.chaconluis006@gmail.com>
2026-09-23 19:44:03 +0200

    Eliminar archivos .gitkeep innecesarios y añadir ConexionBD

---
+++ Trabajo1/src/ejercicio/util/ConexionBD.java
@@ -0,0 +1,26 @@
+package ejercicio.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void cerrarConexion(Connection conexion) {
+        if (conexion != null) {
+            try {
+                conexion.close();
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
+}
